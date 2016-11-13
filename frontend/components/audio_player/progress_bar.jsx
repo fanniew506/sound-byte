dd1a291981e3d578f2a887c80b22950bc0b0f734
Fannie Wang <fannie.wang506@gmail.com>
2016-11-13 15:43:28 -0500

    add progress and duration time on progress bar

--- frontend/components/audio_player/progress_bar.jsx
+++ frontend/components/audio_player/progress_bar.jsx
@@ -6,15 +6,36 @@ class ProgressBar extends React.Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    // this.displayPosition = this.displayPosition.bind(this);
-    // this.displayDuration = this.displayDuration.bind(this);
   }
 
   handleClick(e){
     const position = Math.floor(((e.clientX-262)/600)*this.props.duration);
-    debugger
     this.props.updatePosition(position);
-    debugger
+  }
+
+  displayPosition() {
+    let seconds = Math.floor((this.props.position/1000)%60);
+    let minutes = Math.floor(this.props.position/1000/60);
+    seconds = this.paddedNumber(seconds);
+    minutes = this.paddedNumber(minutes);
+    return <div className="progress-position">{minutes}:{seconds}</div>;
+  }
+
+  paddedNumber(num) {
+    let stringified = num.toString();
+    if (stringified.length === 1) {
+      return "0" + stringified;
+    } else {
+      return stringified;
+    }
+  }
+
+  displayDuration() {
+    let seconds = Math.floor((this.props.duration/1000)%60);
+    let minutes = Math.floor(this.props.duration/1000/60);
+    seconds = this.paddedNumber(seconds);
+    minutes = this.paddedNumber(minutes);
+    return <div className="progress-duration">{minutes}:{seconds}</div>;
   }
 
   render(){
@@ -22,18 +43,19 @@ class ProgressBar extends React.Component {
     let duration = this.props.duration;
 
     return (
-      <div className="progress-container" onClick={ this.handleClick }>
-        <div className='progress-bar'>
-          <div>{this.displayPosition}</div>
-          <div className='audio-progress'
-               style={{width: `${600*(position/duration)}px`}}>
+      <div className="progress-container group">
+        <div className="progress-click" onClick={ this.handleClick }>
+          {this.displayPosition()}
+          <div className='progress-bar'>
+            <div className='audio-progress'
+                 style={{width: `${600*(position/duration)}px`}}>
+            </div>
+            <div className='progress-circle'
+                 style={{left: `${600*(position/duration)}px`}}>
+            </div>
           </div>
-          <div className='progress-circle'
-               style={{left: `${600*(position/duration)}px`}}>
-          </div>
-          <div>{this.displayDuration}</div>
+          {this.displayDuration()}
         </div>
-
       </div>
     );
   }
