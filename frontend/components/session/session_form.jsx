6f95e78b8dbc718dd8f749df461c8cc175a75ed3
Fannie Wang <fannie.wang506@gmail.com>
2016-12-03 17:02:18 -0500

    add session form modals and fixed position

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -5,11 +5,27 @@ import Modal from 'react-modal';
 class SessionForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: "", password: "", errors: [], formType: this.props.formType };
+    this.state = {
+      username: "",
+      password: "",
+      errors: [],
+      formType: this.props.formType,
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updateUsername = this.updateUsername.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
     this.demoLogin = this.demoLogin.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+
+  handleResize(e) {
+    this.setState({
+    windowHeight: window.innerHeight,
+    windowWidth: window.innerWidth
+    });
   }
 
   componentDidUpdate() {
@@ -18,6 +34,11 @@ class SessionForm extends React.Component {
 
   componentDidMount() {
     if (this.props.loggedIn) hashHistory.push("/home");
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('resize', this.handleResize);
   }
 
   handleSubmit(e) {
@@ -57,38 +78,49 @@ class SessionForm extends React.Component {
     }
   }
 
+  modalStyle() {
+    return { width: this.state.windowWidth, height: this.state.windowHeight };
+  }
+
+  handleModalClick() {
+    hashHistory.push("/");
+  }
+
   render() {
     return (
-       <div className="session-form-container">
-         <form onSubmit={this.handleSubmit} className="session-form">
-           <header className="session-form-header">
-             <Link to='/'><h2 className="form-cancel">X</h2></Link>
-            <h2>Log In!</h2>
-           </header>
-           {this.renderErrors()}
-           <br/>
-             <input
-               type="text"
-               placeholder="Username"
+      <div>
+         <div onClick={this.handleModalClick} className="session-modal" style={ this.modalStyle() }></div>
+           <div className="session-form-container">
+             <form onSubmit={this.handleSubmit} className="session-form">
+               <header className="session-form-header">
+                 <Link to='/'><h2 className="form-cancel">X</h2></Link>
+                <h2>Log In!</h2>
+               </header>
+               { this.renderErrors() }
+               <br/>
+                 <input
+                   type="text"
+                   placeholder="Username"
 
-               value={ this.state.username }
-               onChange={ this.updateUsername }/>
-           <br/>
-             <input
-               type="password"
-               placeholder="Password"
-               value={ this.state.password }
-               onChange={ this.updatePassword }/>
-           <br/>
-           <input className="session-submit" type="submit" value="Submit"/>
-           <br/>
-             <div>
-               Don't Have an Account?
-               <Link to="signup"> Signup</Link>
-             </div>
-             <br/>
-             <h2 onClick={this.demoLogin} className="demo-login">Demo Login</h2>
-         </form>
+                   value={ this.state.username }
+                   onChange={ this.updateUsername }/>
+               <br/>
+                 <input
+                   type="password"
+                   placeholder="Password"
+                   value={ this.state.password }
+                   onChange={ this.updatePassword }/>
+               <br/>
+               <input className="session-submit" type="submit" value="Submit"/>
+               <br/>
+                 <div>
+                   Don't Have an Account?
+                   <Link to="signup"> Signup</Link>
+                 </div>
+                 <br/>
+                 <h2 onClick={this.demoLogin} className="demo-login">Demo Login</h2>
+             </form>
+         </div>
        </div>
     );
   }
--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -5,10 +5,25 @@ import Modal from 'react-modal';
 class SignUpForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: "", password: "", errors: [], formType: this.props.formType };
+    this.state = {
+      username: "",
+      password: "",
+      errors: [],
+      formType: this.props.formType,
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updateUsername = this.updateUsername.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  handleResize(e) {
+    this.setState({
+    windowHeight: window.innerHeight,
+    windowWidth: window.innerWidth
+    });
   }
 
   componentDidUpdate() {
@@ -17,6 +32,11 @@ class SignUpForm extends React.Component {
 
   componentDidMount() {
     if (this.props.loggedIn) hashHistory.push("/home");
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('resize', this.handleResize);
   }
 
   handleSubmit(e) {
@@ -52,20 +72,29 @@ class SignUpForm extends React.Component {
     }
   }
 
+  modalStyle() {
+    return { width: this.state.windowWidth, height: this.state.windowHeight };
+  }
+
+  handleModalClick() {
+    hashHistory.push("/");
+  }
+
   render() {
     return (
-       <div className="session-form-container">
-         <form onSubmit={this.handleSubmit} className="session-form">
-           <header className="session-form-header">
-            <Link to='/'><h2 className="form-cancel">X</h2></Link>
-            <h2>Create Account!</h2>
-           </header>
-           {this.renderErrors()}
-           <br/>
+      <div>
+        <div onClick={this.handleModalClick} className="session-modal" style={ this.modalStyle() }></div>
+        <div className="session-form-container">
+          <form onSubmit={this.handleSubmit} className="session-form">
+            <header className="session-form-header">
+              <Link to='/'><h2 className="form-cancel">X</h2></Link>
+              <h2>Create Account!</h2>
+            </header>
+              {this.renderErrors()}
+              <br/>
              <input
                type="text"
                placeholder="Username"
-
                value={ this.state.username }
                onChange={ this.updateUsername }/>
            <br/>
@@ -83,6 +112,7 @@ class SignUpForm extends React.Component {
              </div>
          </form>
        </div>
+      </div>
     );
   }
 }
