04d931a4f3d182c15a55ce38a7b787e844660c79
Fannie Wang <fannie.wang506@gmail.com>
2016-12-06 14:28:05 -0500

    add error reducer

---
+++ frontend/reducers/error_reducer.js
@@ -0,0 +1,18 @@
+import {
+  RECEIVE_ERRORS, CLEAR_ERRORS } from '../actions/error_actions';
+import merge from 'lodash/merge';
+
+const defaultState = Object.freeze({
+  errors: []
+});
+
+export default (state = defaultState, action) => {
+  switch(action.type) {
+    case RECEIVE_ERRORS:
+      return merge({}, defaultState, { errors: action.errors });
+    case CLEAR_ERRORS:
+      return defaultState;
+    default:
+      return state;
+  }
+};
